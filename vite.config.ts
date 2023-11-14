558bcc2ccd093be2bca0ba56ae390ac67a56a452
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-11-14 17:45:29 +0700

    chore: configure PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,24 @@ import million from 'million/compiler';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  base: '/',
+  manifest: {
+    name: 'Vite React App Template',
+    short_name: 'vite-react-ts',
+    theme_color: '#FFFFFF',
+    lang: 'en',
+    start_url: '/',
+    background_color: '#FFFFFF',
+    dir: 'ltr',
+    display: 'standalone',
+    orientation: 'portrait',
+    prefer_related_applications: false,
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +33,7 @@ export default defineConfig({
       eslint: { lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"' },
     }),
     tsConfigPaths(),
+    VitePWA(pwaOptions),
   ],
   server: {
     open: true,
