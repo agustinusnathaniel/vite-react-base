a49fc05bf85b8cf9cea4e48350bb728cc1ac1367
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-11-16 23:37:17 +0700

    chore: update tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react-swc';
 import million from 'million/compiler';
-import tsConfigPaths from 'vite-tsconfig-paths';
-import { PluginOption, defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
 import { visualizer } from 'rollup-plugin-visualizer';
+import type { PluginOption } from 'vite';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
