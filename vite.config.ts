9aa5bf787761c48a9b74fcb0c071c634a0b8890e
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-12-08 14:54:34 +0700

    chore: configure PWA (#1)
    * chore: configure PWA
    
    * chore: configure PWA

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,33 @@ import { visualizer } from 'rollup-plugin-visualizer';
 import type { PluginOption } from 'vite';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
+import type { VitePWAOptions } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsConfigPaths from 'vite-tsconfig-paths';
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  base: '/',
+  manifest: {
+    name: 'Vite React App Template',
+    short_name: 'vite-react-ts',
+    theme_color: '#FFFFFF',
+    lang: 'en',
+    start_url: '/',
+    background_color: '#FFFFFF',
+    dir: 'ltr',
+    display: 'standalone',
+    orientation: 'natural',
+    prefer_related_applications: false,
+    icons: [
+      {
+        src: '/assets/favicon.svg',
+        purpose: 'maskable any',
+        sizes: '48x48 72x72 96x96 128x128 256x256',
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +45,7 @@ export default defineConfig({
       },
     }),
     tsConfigPaths(),
+    VitePWA(pwaOptions),
     visualizer({ template: 'sunburst' }) as unknown as PluginOption,
   ],
   server: {
